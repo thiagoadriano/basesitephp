5b6991ee055233de164edd98f25da7776bdd7677
Thiago Adriano <thiago.s.adriano@gmail.com>
2016-04-25 01:24:18 -0300

    Ajustado arquivo de Meta

--- controller/metaController.php
+++ controller/metaController.php
@@ -3,49 +3,60 @@ require_once 'mainController.php';
 
 class MetasController
 {
-    protected $titleHome;
-
-
-    protected $DescriptionHome;
-
-
+    protected $title = "";
+    private $description = "";
     private $pageAtual;
+    private $default = array(
+        'title' => "PlugTelecom - Soluções Simples para o seu negócio",
+        'description' => "Soluções Simples para o seu negócio"
+    );
+    private $error = array(
+        'page' => "404",
+        'title' => "Página não encontrada - PlugTelecom",
+        'description' => "Soluções Simples para o seu negócio"
+    );
+    private $infoPages = array(
+        array(
+            'page' => "home",
+            'title' => "Início - PlugTelecom",
+            'description' => "Soluções Simples para o seu negócio"
+        )
+    );
 
     public function __construct(){
-        $this->titleHome = "";
-
-        $this->DescriptionHome = "";
-
         $this->pageAtual = new mainController();
+        $this->setTitleDescription();
+    }
+
+    private function setTitleDescription()
+    {
+        for($i = 0; $i < count($this->infoPages); $i++){
+            if($this->infoPages[$i]['page'] == $this->pageAtual->getPage()){
+                $this->title = $this->infoPages[$i]['title'];
+                $this->description = $this->infoPages[$i]['description'];
+                return;
+            }else if($this->pageAtual->getPage() == $this->error['page']){
+                $this->title = $this->error['title'];
+                $this->description = $this->error['description'];
+                return;
+            }
+        }
+        if($this->pageAtual->getExistsFile()){
+            $this->title = $this->default['title'];
+            $this->description = $this->default['description'];
+        }else{
+            $this->title = $this->error['title'];
+            $this->description = $this->error['description'];
+        }
 
     }
 
-    public function setTitle(){
-        switch($this->pageAtual->getPage()){
-
-            case '404':
-                return $this->title404;
-            break;
-
-            default:
-                return $this->titleHome;
-            break;
-
-        }
+    public function getTitle(){
+        return $this->title;
     }
 
-    public function setDescription(){
-        switch($this->pageAtual->getPage()){
-
-            case '404':
-                return $this->Description404;
-                break;
-
-            default:
-                return $this->DescriptionHome;
-                break;
-
-        }
+    public function getDescription(){
+        return $this->description;
     }
 
 }
