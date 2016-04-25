cea829e96ea85ac0bc1bde4d1321e5b4ceecb6d4
Thiago Adriano <thiago.s.adriano@gmail.com>
2016-04-25 01:23:19 -0300

    Ajustado a classe de página para recorrer se arquivo existe

--- controller/mainController.php
+++ controller/mainController.php
@@ -6,12 +6,18 @@ class mainController
     private $URL = "";
     private $pageInit = "";
     private $pageExplode = array();
+    private $dir = "";
+    private $fileExists = false;
 
     public  function __construct()
     {
         $this->pageInit = $_SERVER['REQUEST_URI'];
-        $this->pageExplode = explode('/', preg_replace('%^(.*)\?.*$%','$1',$this->pageInit));
+        $this->pageInit = preg_replace('%^(.*)\?.*$%','$1',$this->pageInit);
+        $this->pageInit = preg_replace('%/$%', '', $this->pageInit);
+        $this->pageInit = preg_replace('%^/%', '', $this->pageInit);
+        $this->pageExplode = explode('/', $this->pageInit);
         $this->page = end($this->pageExplode);
+        $this->checkExistsFile();
     }
     public function getPage()
     {
@@ -20,7 +26,11 @@ class mainController
 
     public function updatePage()
     {
-        require $this->exixtsPage();
+        if($this->page == ""){
+            require 'views/home.php';
+        }else{
+            require $this->existsPage();
+        }
     }
 
     public function getURL()
@@ -28,12 +38,35 @@ class mainController
         echo $this->URL;
     }
 
-    private function exixtsPage()
+    private function existsPage()
     {
-        if (file_exists('views/' . $this->page . '.php')) {
-            return 'views/' . $this->page . '.php';
+        if ($this->fileExists) {
+            return $this->dir . $this->page . '.php';
         } else {
             return 'views/404.php';
         }
+    }
+
+    private function checkExistsFile()
+    {
+        $this->dir = $this->prepareDir();
+        if (file_exists($this->dir . $this->page . '.php')) {
+            $this->fileExists = true;
+        }
+    }
+
+    private function prepareDir()
+    {
+        $dirReturn = "views/";
+        if (count($this->pageExplode) > 1) {
+            for ($i = 0; $i < count($this->pageExplode) - 1; $i++) {
+                $dirReturn .= $this->pageExplode[$i] . "/";
+            }
+        }
+        return $dirReturn;
+    }
+
+    public function getExistsFile(){
+        return $this->fileExists;
     }
 }
